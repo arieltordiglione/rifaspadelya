ae5a04a4f6cc9ab790efac16ca12051e12ccad8d
Ariel Tordiglione <91021925+arieltordiglione@users.noreply.github.com>
2023-07-02 04:59:38 -0300

    Add files via upload

---
+++ text.js
@@ -0,0 +1,21 @@
+/* var container = document.getElementById('tabla');
+
+// Generar los divs con los números del 00 al 99
+for (var i = 0; i < 100; i++) {
+	var div = document.createElement('div');
+	div.className = 'numero';
+	div.textContent = ('0' + i).slice(-2); // Asegura que siempre tenga 2 dígitos
+	container.appendChild(div);
+}
+
+// Obtener todos los elementos div con la clase "numero"
+var elementos = document.querySelectorAll('.numero');
+
+// Recorrer los elementos obtenidos
+for (var i = 0; i < elementos.length; i++) {
+	var estructuraHTML = elementos[i].outerHTML;
+	console.log(estructuraHTML);
+}
+
+//despues lo copie de la consolta, lo puse en excel y filtre el text.17blabla de la columna
+ */
